fafe1ce8942ad970673215939cddc655fd341ecc
SiewJun <siewkhaijun@gmail.com>
2024-09-02 02:55:36 +0800

    corrected link in authnav and nav

--- client/src/_components/nav/AuthNavBar.jsx
+++ client/src/_components/nav/AuthNavBar.jsx
@@ -6,7 +6,7 @@ function AuthNavBar() {
   return (
     <>
       <div className="flex w-full items-center justify-between px-5 py-3 md:px-4">
-        <Link href="/">
+        <Link to="/">
           <EcoSyncLogo />
         </Link>
         <ThemeSwitcher />
--- client/src/_components/nav/NavBar.jsx
+++ client/src/_components/nav/NavBar.jsx
@@ -39,7 +39,7 @@ const components = [
 function NavBar() {
   return (
     <div className="flex w-full items-center justify-between px-5 py-3 md:px-4">
-      <Link href="/">
+      <Link to="/">
         <EcoSyncLogo/>
       </Link>
       <div className="md:hidden">
