5a520659e62246883e2b586a6bb88df30b4b6edc
SiewJun <siewkhaijun@gmail.com>
2024-09-29 20:47:39 +0800

    fixed loading

--- client/src/_components/company/CompanyProfile.jsx
+++ client/src/_components/company/CompanyProfile.jsx
@@ -37,7 +37,11 @@ const CompanyProfile = () => {
   }, []);
 
   if (!profile) {
-    return <p>Loading...</p>;
+    return (
+      <div className="flex justify-center items-center h-64">
+        <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-primary"></div>
+      </div>
+    );
   }
 
   const onDelete = async (id) => {
